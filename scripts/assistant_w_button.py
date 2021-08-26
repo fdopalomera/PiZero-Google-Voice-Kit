547e566828f73380688135ca7129ffb43f4e2386
Fernando Palomera <fernando@MacBook-Pro-de-Fernando.local>
2021-08-25 23:16:04 -0400

    change to es-MX locale

--- scripts/assistant_w_button.py
+++ scripts/assistant_w_button.py
@@ -1,96 +1,37 @@
 #!/usr/bin/env python3
-
-"""Activates the Google Assistant with either a hotword or a button press, using the
-Google Assistant Library.
-
-The Google Assistant Library has direct access to the audio API, so this Python
-code doesn't need to record audio.
-
-.. note:
-
-    Hotword detection (such as "Okay Google") is supported only with Raspberry Pi 2/3.
-    If you're using a Pi Zero, this code works but you must press the button to activate
-    the Google Assistant.
-"""
-
+import argparse
 import logging
-import platform
+import signal
 import sys
-import threading
-
-from google.assistant.library.event import EventType
-
-from aiy.assistant import auth_helpers
-from aiy.assistant.library import Assistant
-from aiy.board import Board, Led
+from aiy.assistant.grpc import AssistantServiceClientWithLed
+from aiy.board import Board
 
 
-class MyAssistant:
-    """An assistant that runs in the background.
-
-    The Google Assistant Library event loop blocks the running thread entirely.
-    To support the button trigger, we need to run the event loop in a separate
-    thread. Otherwise, the on_button_pressed() method will never get a chance to
-    be invoked.
-    """
-
-    def __init__(self):
-        self._task = threading.Thread(target=self._run_task)
-        self._can_start_conversation = False
-        self._assistant = None
-        self._board = Board()
-        self._board.button.when_pressed = self._on_button_pressed
-
-    def start(self):
-        """
-        Starts the assistant event loop and begins processing events.
-        """
-        self._task.start()
-
-    def _run_task(self):
-        credentials = auth_helpers.get_assistant_credentials()
-        with Assistant(credentials) as assistant:
-            self._assistant = assistant
-            for event in assistant.start():
-                self._process_event(event)
-
-    def _process_event(self, event):
-        logging.info(event)
-        if event.type == EventType.ON_START_FINISHED:
-            self._board.led.status = Led.BEACON_DARK  # Ready.
-            self._can_start_conversation = True
-            # Start the voicehat button trigger.
-            logging.info('Say "OK, Google" or press the button, then speak. '
-                         'Press Ctrl+C to quit...')
-
-        elif event.type == EventType.ON_CONVERSATION_TURN_STARTED:
-            self._can_start_conversation = False
-            self._board.led.state = Led.ON  # Listening.
-
-        elif event.type == EventType.ON_END_OF_UTTERANCE:
-            self._board.led.state = Led.PULSE_QUICK  # Thinking.
-
-        elif (event.type == EventType.ON_CONVERSATION_TURN_FINISHED
-              or event.type == EventType.ON_CONVERSATION_TURN_TIMEOUT
-              or event.type == EventType.ON_NO_RESPONSE):
-            self._board.led.state = Led.BEACON_DARK  # Ready.
-            self._can_start_conversation = True
-
-        elif event.type == EventType.ON_ASSISTANT_ERROR and event.args and event.args['is_fatal']:
-            sys.exit(1)
-
-    def _on_button_pressed(self):
-        # Check if we can start a conversation. 'self._can_start_conversation'
-        # is False when either:
-        # 1. The assistant library is not yet ready; OR
-        # 2. The assistant library is already in a conversation.
-        if self._can_start_conversation:
-            self._assistant.start_conversation()
+def volume(string):
+    value = int(string)
+    if value < 0 or value > 100:
+        raise argparse.ArgumentTypeError('Volume must be in [0...100] range.')
+    return value
 
 
 def main():
-    logging.basicConfig(level=logging.INFO)
-    MyAssistant().start()
+    logging.basicConfig(level=logging.DEBUG)
+    signal.signal(signal.SIGTERM, lambda signum, frame: sys.exit(0))
+
+    parser = argparse.ArgumentParser(description='Assistant service with Button')
+    parser.add_argument('--language', default='es-MX')
+    parser.add_argument('--volume', type=volume, default=100)
+    args = parser.parse_args()
+
+    with Board() as board:
+        assistant = AssistantServiceClientWithLed(board=board,
+                                                  volume_percentage=args.volume,
+                                                  language_code=args.language)
+        while True:
+            logging.info('Press button to start conversation...')
+            board.button.wait_for_press()
+            logging.info('Conversation started!')
+            assistant.conversation()
 
 
 if __name__ == '__main__':
